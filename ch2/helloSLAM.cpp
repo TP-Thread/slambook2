a5e22240947f5ccc657a89618b7afc16d53841f5
TP-Thread <TP-Thread@qq.com>
2023-10-28 15:06:05 +0800

    0.初次提交

---
+++ ch2/helloSLAM.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "libHelloSLAM.h"
+using namespace std;
+
+int main(int argc, char **argv) 
+{
+    //cout << "Hello SLAM!" << endl;
+
+    printHello();
+
+    return 0;
+}
